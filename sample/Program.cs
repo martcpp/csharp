6f64d0d676279ba00b1a4125ea00d52eeb39490a
martcpp <mordecaiemmanueletukudo@gmail.com>
2022-12-18 00:27:15 +0100

    hhu

---
+++ sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine(Math.Cbrt(8.7));
+           
+            
+        }
+    }
+}
